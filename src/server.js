eb0bf254425ad9416aa9a586369ae71f3d1dfd1a
Bogdan Birau <birau.bogdan92@gmail.com>
2021-12-07 17:48:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ const whiteList = [process.env.FE_LOCAL_URL, process.env.FE_REMOTE_URL];
 
 const corsOptions = {
   origin: function (origin, next) {
-    console.log({ origin });
+    console.log(arguments);
     if (!origin || whiteList.indexOf(origin) !== -1) {
       next(null, true);
     } else {
